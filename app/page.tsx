28e84313bdcc96a36257bdf236f0f2a3edd296f6
danielrhodesy <danielrhodesnz@gmail.com>
2024-02-19 12:15:43 +1300

    Added Uploader files

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Uploader from './ui/uploader/uploader'
 
 
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
+      <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
+        <Uploader />
+      </div>
     </main>
   );
 }
